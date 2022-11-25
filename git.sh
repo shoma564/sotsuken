072227b968e597bcb6d28a6df377b922b2eb2acb
shoma564 <shomaigu@gmail.com>
2022-11-25 05:49:45 +0000

    ${mozi} ${today}

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-  kai=0
+  kai=1
   today=`date "+%Y%m%d"`
   mozi="自動プッシュ"
   su="回目のプッシュ完了"
@@ -9,5 +9,6 @@ do
   git commit -m '${mozi} ${today}'
   git push
   echo $kai$su
+  kai=kai+1
   sleep 600
 done
