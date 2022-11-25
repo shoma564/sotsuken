1f0ec9c9cd4a4bc279b304b83fb3041375a69224
shoma564 <shomaigu@gmail.com>
2022-11-25 06:15:43 +0000

    python

--- git.sh
+++ git.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+cd /home/shoma/sotsuken/
 while true
 do
   kai=1
-  today=`date "+%Y%m%d"`
+  today=`date "+%Y%m%d%k%M"`
   mozi="自動プッシュ"
   su="回目のプッシュ完了"
-  git add *
-  git commit -m '$mozi $today'
-  git push
+  git add /home/shoma/sotsuken/*
+  git commit -m ${mozi} ${today}
+  git push main
   echo $kai$su
   kai=kai+1
-  sleep 600
+  sleep 1200
 done
