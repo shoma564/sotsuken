32d7e5450159a4cb53ba20445158dbf9fc2b6e0d
shoma564 <shomaigu@gmail.com>
2022-12-02 07:01:31 +0000

    sshまじで完成

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 docker-compose down
 
-#docker-compose down --rmi all --volumes --remove-orphans
-#docker build ./ubpy -t shomaigu/sotsuken-python:latest
-#docker push shomaigu/sotsuken-python:latest
-#docker build ./ubuntuman -t shomaigu/sotsuken-ubuntu-manage:latest
-#docker push shomaigu/sotsuken-ubuntu-manage:latest
-#docker build ./ubuntunmap -t shomaigu/sotsuken-ubuntu-nmap:latest
-#docker push shomaigu/sotsuken-ubuntu-nmap:latest
+docker-compose down --rmi all --volumes --remove-orphans
+docker build ./ubpy -t shomaigu/sotsuken-python:latest
+docker push shomaigu/sotsuken-python:latest
+docker build ./ubuntuman -t shomaigu/sotsuken-ubuntu-manage:latest
+docker push shomaigu/sotsuken-ubuntu-manage:latest
+docker build ./ubuntunmap -t shomaigu/sotsuken-ubuntu-nmap:latest
+docker push shomaigu/sotsuken-ubuntu-nmap:latest
 
 docker-compose build
 docker-compose up
