9c074447c7016075142df909c1e194d39cd87849
shoma564 <shomaigu@gmail.com>
2022-12-21 14:41:00 +0000

    docker,composeの自動インストール

--- docker/start.py
+++ docker/start.py
@@ -1,6 +1,19 @@
-import os
+import os, shutil
 
 os.system('cd ./template && ./reset.sh')
+
+def install():
+	docker = shutil.which("docker")
+	docker = str(docker)
+	if docker == "None":
+		os.system('apt -y update')
+		os.system('apt -y install docker')
+
+	dockerc = shutil.which("docker-compose")
+	dockerc = str(dockerc)
+	if dockerc == "None":
+		os.system('apt -y update')
+		os.system('apt -y install docker-compose')
 
 def scan():
 	file_name = "./ubuntunmap/sh/script.sh"
@@ -31,14 +44,14 @@ def scan():
 
 
 def docker():
-	#os.system('apt -y update')
-	#os.system('apt -y install docker docker-compose')
 	os.system('docker-compose build')
 	os.system('docker-compose up -d')
 
 
 
+###### ./share/result.txtが出来上がるまで、無限ループで参照し続ける。もし、存在が確認されたら、処理終了。
 
 
+install()
 scan()
 docker()
--- docker/template/reset.sh
+++ docker/template/reset.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+rm  ../share/result.txt
 cp ./ubuntunmap/sh/script.sh ../ubuntunmap/sh/script.sh
