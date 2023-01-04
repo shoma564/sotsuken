ddec40c06c5f58e1edafac8921ebb878d4b21dd4
shoma564 <shomaigu@gmail.com>
2023-01-04 03:59:33 +0000

    20230104 359

--- git.sh
+++ git.sh
@@ -6,8 +6,9 @@ do
   mozi="自動プッシュ"
   su="回目のプッシュ完了"
   echo ${mozi} ${today}
+  git checkout main
   git add /home/shoma/sotsuken/*
-  git commit -m '${mozi} ${today}'
+  git commit -m "${today}"
   git push main
   echo $kai$su
   kai=kai+1
