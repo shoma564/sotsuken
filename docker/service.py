1513a9499663f5dcaa08649110a14652773397ee
shoma564 <shomaigu@gmail.com>
2023-01-13 03:02:29 +0000

    20230113 302

--- docker/service.py
+++ docker/service.py
@@ -1,14 +1,66 @@
-#import aa
+import time
 
-def check_string():
-    with open('temp.txt') as temp_f:
+print("検出サービスの整理中")
+
+for number in range(6):
+	time.sleep(0.5)
+	print(".")
+print("\n\n")
+
+==================================サービスの検出=========================================
+def check_mysql():
+    with open('./share/mg_result.txt') as temp_f:
         datafile = temp_f.readlines()
     for line in datafile:
-        if 'blabla' in line:
-            return True # The string is found
-    return False  # The string does not exist in the file
+        if 'mysql' in line:
+            return True
+    return False
 
-if check_string():
-    print('True')
+
+def check_nginx():
+    with open('./share/mg_result.txt') as temp_f:
+        datafile = temp_f.readlines()
+    for line in datafile:
+        if 'nginx' in line:
+            return True
+    return False
+
+
+def check_apache():
+    with open('./share/mg_result.txt') as temp_f:
+        datafile = temp_f.readlines()
+    for line in datafile:
+        if 'apache' in line:
+            return True
+    return False
+
+
+
+
+====================================サービスの移行=======================================================
+
+======================================================================================================
+
+check_mysql()
+check_nginx()
+check_apache()
+
+
+
+if check_mysql():
+    print('Mysql_True')
+    mysql()
 else:
-    print('False')
+    print('Mysql_False')
+
+if check_nginx():
+    print('nginx_True')
+    nginx()
+else:
+    print('nginx_False')
+
+if check_apache():
+    print('apache_True')
+    apache()
+else:
+    print('apache_False')
